5c359a1fba684fb64b1f1c832ee0a253ae761725
Aditya Gupta <adiiigupta7392@gmail.com>
2026-09-11 14:44:10 +0530

    initial

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth.js");
+const eventRoutes = require("./routes/events.js");
+const bookingRoutes = require("./routes/bookings.js");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(`/api/auth`, authRoutes);
+
+app.use(`/api/events`, eventRoutes);
+app.use(`/api/booking`, bookingRoutes);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
